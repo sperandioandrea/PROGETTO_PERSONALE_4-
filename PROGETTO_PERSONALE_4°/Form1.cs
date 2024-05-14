ecf054ce6f7fad0a4965cffced78960a20b90bac
Sperandio.20200 <sperandio.andrea.studente@itispaleocapa.it>
2024-05-14 12:26:48 +0200

    creazione classe CalcoloFabbisognoCaloricoGiornaliero

--- PROGETTO_PERSONALE_4°/Form1.cs
+++ PROGETTO_PERSONALE_4°/Form1.cs
@@ -16,5 +16,10 @@ namespace PROGETTO_PERSONALE_4_
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
