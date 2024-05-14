a14b98e696fbbc89d2103943a77a5c69c1247df0
Sperandio.20200 <sperandio.andrea.studente@itispaleocapa.it>
2024-05-14 12:34:49 +0200

    Attributi classe CalcoloFabbisognoCaloricoGiornaliero

--- PROGETTO_PERSONALE_4°/CalcoloFabbisognoCaloricoGiornaliero.cs
+++ PROGETTO_PERSONALE_4°/CalcoloFabbisognoCaloricoGiornaliero.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace PROGETTO_PERSONALE_4_
 {
-    internal class CalcoloFabbisognoCaloricoGiornaliero
+    public enum AttivitaFisica { Sedentario, ModeratamenteAttivo, Attivo }
+
+    public class CalcoloFabbisognoCaloricoGiornaliero
     {
+        //ATTRIBUTI
+        public string Sesso { get; set; }
+        public double Peso { get; set; }
+        public double Altezza { get; set; }
+        public int Eta { get; set; }
+        public AttivitaFisica LivelloAttivita { get; set; }
+
     }
 }
